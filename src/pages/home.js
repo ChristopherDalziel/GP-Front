33577d5cb8eee27b35af0255a79ed5637843b164
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-01-14 22:28:53 +1100

    added folder for css and assets, imported sass although unintended for use can uninstall in next push, added background functionality, in the future we need to chose new images that are big enough to allow the functionality.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import Nav from '../components/Nav';
+import React from "react";
+import Nav from "../components/Nav";
+import "../css/Home.css";
 
 class Home extends React.Component {
   render() {
     return (
       <>
-        <div>
-          <Nav />
+        <Nav />
+        <div class="slider">
+          <div class="load"></div>
+          <div class="content">
+            <div class="principal">
+              <h1>General Practice</h1>
+              <p>
+                <button>Contact Us</button>
+                <button>Book Now</button>
+              </p>
+            </div>
+          </div>
         </div>
       </>
-    )
-  };
+    );
+  }
 }
 
 export default Home;
