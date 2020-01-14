0145093630ef65e7df72017f48984cf25f339580
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-01-14 15:06:51 +1100

    added nav bar and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <>
-      <p>mern final 2.0</p>
-    </>
-  );
+import Home from './pages/home';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+      </BrowserRouter>
+      </>
+    )
+  };
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './Nav.css';
+
+class Nav extends Component {
+  render() {
+    return (
+        <div className="nav">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+          </ul>
+        </div>
+    )
+  }
+}
+
+export default Nav;
