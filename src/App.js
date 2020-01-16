2c3ff734a5a8ca66072ced0b26ca8fada1c807ab
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-01-16 17:15:29 +1100

    Adding to nav, + routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./pages/home";
 import Contact from "./pages/contact";
 import Profile from "./pages/profile";
 import About from "./pages/about";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
 
 class App extends React.Component {
   render() {
@@ -13,6 +15,8 @@ class App extends React.Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="signup" component={Signup} />
+            <Route path="login" component={Login} />
             <Route path="/contact" component={Contact} />
             <Route path="/profile" component={Profile} />
             <Route path="/about" component={About} />
