6c067aa831a098f3b7f4f7619f0132f40d0c7974
Cai heng <ianyaheng822@gmail.com>
2020-01-20 15:12:10 +1100

    add the doctos-card

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from "../components/Nav";
 
 import "../css/About.css";
-import drPortrait from "../assets/dr.jpeg";
+import doctor from "../assets/doctor.jpg";
 
 class About extends React.Component {
   render() {
@@ -12,7 +12,7 @@ class About extends React.Component {
         <div className="about-us">
           <div className="intro">
             <div className="about-image">
-            
+  
             </div>
             <div className="about-infor">
               <h1>Welcome to Klinic Doctor Leong</h1>
@@ -23,29 +23,31 @@ class About extends React.Component {
               
             </div>
           </div>
+          <div className="doc-infors">
+            <h1>Our Doctors</h1>
+            <p>Klinic Doctor Leong are dedicated to providing you the best possible care. On this page you can find a GP by name, gender, location, languages spoken or medial interest.</p>
+          </div>
 
           <div className="doctors">
             <div className="doctor-card">
-              <img src="../assets/dr.jpeg" alt=""/>
-              <h4>Heng</h4>
+              <img src={doctor} alt=""/>
+              <h3>Heng</h3>
               <h4>Doctor</h4>
               <p>Dr Danqualified from the University of Colombo in 20019, has been working in the emergency departments in Singapore and Sydney for six years till 2016.  Since 2017 he has been a GP and is currently a trainee registrar of FACRRM</p>
             </div>
             <div className="doctor-card">
-              <img src="../assets/dr.jpeg" alt=""/>
-              <h4>Nahn</h4>
+              <img src={doctor}  alt=""/>
+              <h3>Nahn</h3>
               <h4>Doctor</h4>
               <p>Dr Danqualified from the University of Colombo in 20019, has been working in the emergency departments in Singapore and Sydney for six years till 2016.  Since 2017 he has been a GP and is currently a trainee registrar of FACRRM</p>
             </div>
             <div className="doctor-card">
-              <img src="../assets/dr.jpeg" alt=""/>
-              <h4>Chris</h4>
+              <img src={doctor}  alt=""/>
+              <h3>Chris</h3>
               <h4>receptionist</h4>
               <p>Dr Danqualified from the University of Colombo in 20019, has been working in the emergency departments in Singapore and Sydney for six years till 2016.  Since 2017 he has been a GP and is currently a trainee registrar of FACRRM</p>
             </div>
           </div>
-          
-
         </div>
         
       </>
