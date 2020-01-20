f92aacdfee1240c6283a65b6e95b9524d2bdc342
Cai heng <ianyaheng822@gmail.com>
2020-01-20 14:47:02 +1100

    add the about us information

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,29 +9,45 @@ class About extends React.Component {
     return (
       <>
         <Nav />
-        <div className="about">
-          <div className="container">
-            <div className="imageAbout">
-              <div className="employeeCard">
-                <img src={drPortrait} alt="of the Doctor" />
-                <h4>Dr. Nhan</h4>
-                <p className="employeeAbout"> Hello...</p>
-              </div>
-              <div className="employeeCard">
-                <img src={drPortrait} alt="of the Doctor" />
-                <h4>Dr. Heng</h4>
-                <p className="employeeAbout">I'm Psyduck</p>
-              </div>
+        <div className="about-us">
+          <div className="intro">
+            <div className="about-image">
+            
             </div>
-            <div className="content-about">
+            <div className="about-infor">
+              <h1>Welcome to Klinic Doctor Leong</h1>
               <div>
-                <h1>About Us</h1>
-                <h5>Klinik</h5>
-                <p>This is where info about the clinic will go</p>
+                <p>Medical One GP's are dedicated to providing you the best possible care. On this page you can find a GP by name, gender, location, languages spoken or medial interest.</p>
+                <p> Medical One GP's are dedicated to providing you the best possible care. On this page you can find a GP by name, gender, location, languages spoken or medial interest.</p>
               </div>
+              
             </div>
           </div>
+
+          <div className="doctors">
+            <div className="doctor-card">
+              <img src="../assets/dr.jpeg" alt=""/>
+              <h4>Heng</h4>
+              <h4>Doctor</h4>
+              <p>Dr Danqualified from the University of Colombo in 20019, has been working in the emergency departments in Singapore and Sydney for six years till 2016.  Since 2017 he has been a GP and is currently a trainee registrar of FACRRM</p>
+            </div>
+            <div className="doctor-card">
+              <img src="../assets/dr.jpeg" alt=""/>
+              <h4>Nahn</h4>
+              <h4>Doctor</h4>
+              <p>Dr Danqualified from the University of Colombo in 20019, has been working in the emergency departments in Singapore and Sydney for six years till 2016.  Since 2017 he has been a GP and is currently a trainee registrar of FACRRM</p>
+            </div>
+            <div className="doctor-card">
+              <img src="../assets/dr.jpeg" alt=""/>
+              <h4>Chris</h4>
+              <h4>receptionist</h4>
+              <p>Dr Danqualified from the University of Colombo in 20019, has been working in the emergency departments in Singapore and Sydney for six years till 2016.  Since 2017 he has been a GP and is currently a trainee registrar of FACRRM</p>
+            </div>
+          </div>
+          
+
         </div>
+        
       </>
     );
   }
