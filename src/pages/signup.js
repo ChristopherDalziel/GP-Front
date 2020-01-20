4cf061882f6c52e3044e196301f73b9c568619e1
Cai heng <ianyaheng822@gmail.com>
2020-01-20 12:28:07 +1100

    add useState for user sign up

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,8 +1,20 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState} from "react";
 import Nav from "../components/Nav";
 import "../css/signUp.css";
 
-function signUp() {
+const SignUp =() =>{
+
+  const [formData,setFormData]=useState({
+    firstName:'',
+    lasrtName:'',
+    email:'',
+    phone:'',
+    password:'',
+    password2:''
+  });
+
+  const {firstName,lasrtName,email,phone,password,password2}= formData
+
   return (
     <Fragment>
       <Nav />
@@ -13,67 +25,71 @@ function signUp() {
             <div>
               <h1>New to Klinic Doctor Leong?</h1>
               <h1>Sign UP Now</h1>
+              <form action="">
+                <div className="input-wrapper-1">
+                  <label>First Name</label> <br />
+                  <input
+                    type="text"
+                    placeholder="Enter Username"
+                    name="firstName"
+                    required
+                  />
+                </div>
+                <div className="input-wrapper-2">
+                  <label>Last Name</label>
+                  <br />
+                  <input
+                    type="text"
+                    placeholder="Enter Password"
+                    name="pslasrtNamew"
+                    required
+                  />
+                </div>
+                <div className="input-wrapper-3">
+                  <label>Email</label>
+                  <br />
+                  <input
+                    type="text"
+                    placeholder="Enter Password"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="input-wrapper-4">
+                  <label>Phone Number</label>
+                  <br />
+                  <input
+                    type="text"
+                    placeholder="Enter Password"
+                    name="phone"
+                    required
+                  />
+                </div>
+                <div className="input-wrapper-5">
+                  <label>Password</label>
+                  <br />
+                  <input
+                    type="password"
+                    placeholder="Enter Password"
+                    name="password"
+                    required
+                  />
+                </div>
+                <div className="input-wrapper-6">
+                  <label>Confirm Password</label>
+                  <br />
+                  <input
+                    type="password"
+                    placeholder="Enter Password"
+                    name="password2"
+                    required
+                  />
+                </div>
+                <button type="submit">Sign Up</button>
 
-              <div className="input-wrapper-1">
-                <label>First Name</label> <br />
-                <input
-                  type="text"
-                  placeholder="Enter Username"
-                  name="uname"
-                  required
-                />
-              </div>
-              <div className="input-wrapper-2">
-                <label>Last Name</label>
-                <br />
-                <input
-                  type="password"
-                  placeholder="Enter Password"
-                  name="psw"
-                  required
-                />
-              </div>
-              <div className="input-wrapper-3">
-                <label>Email</label>
-                <br />
-                <input
-                  type="password"
-                  placeholder="Enter Password"
-                  name="psw"
-                  required
-                />
-              </div>
-              <div className="input-wrapper-4">
-                <label>Phone Number</label>
-                <br />
-                <input
-                  type="password"
-                  placeholder="Enter Password"
-                  name="psw"
-                  required
-                />
-              </div>
-              <div className="input-wrapper-5">
-                <label>Password</label>
-                <br />
-                <input
-                  type="password"
-                  placeholder="Enter Password"
-                  name="psw"
-                  required
-                />
-              </div>
-              <div className="input-wrapper-6">
-                <label>Confirm Password</label>
-                <br />
-                <input
-                  type="password"
-                  placeholder="Enter Password"
-                  name="psw"
-                  required
-                />
-              </div>
-              <button type="submit">Sign In</button>
+              </form>
+
+              
             </div>
           </div>
         </div>
@@ -82,4 +98,4 @@ function signUp() {
   );
 }
 
-export default signUp;
+export default SignUp;
