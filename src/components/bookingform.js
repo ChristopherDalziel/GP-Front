7d37208ca9ede8474d2fa737a612b889516f17b7
Nhan Dang <nhantdang@gmail.com>
2020-01-22 09:38:38 +1100

    updated booking form

--- src/components/bookingform.js
+++ src/components/bookingform.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {Field, reduxForm} from 'redux-form';
+import {Field, reduxForm, formValueSelector} from 'redux-form';
+import { connect } from 'react-redux';
 import SelectDateTime from './datepicker';
+import PropTypes from 'prop-types';
 import DatePicker, { registerLocale, setDefaultLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import normalizePhone from './normalizePhone';
@@ -46,12 +48,39 @@ class BookingForm extends React.Component {
   };
  
   handleChange = (date) => {
+    // console.log(date)
     this.setState({
       startDate: date
     });
+    // this.props.input.onChange(date);
+   
+   
   };
 
+  datePicker = ({ input, meta: { touched, error } }) => {
+    console.log(input)
+
+    return (
+    <div>
+      <DatePicker 
+      selected={this.state.startDate}
+      onChange={this.handleChange}
+      locale="en-GB"
+      showTimeSelect
+      timeFormat="hh:mm aa"
+      timeIntervals={15}
+      dateFormat="dd-MMMM-yyyy hh:mm aa"
+      placeholderText="Click to select"
+    />
+    {touched && error && <span className="error_field">{error}</span>}
+        </div>
+
+    )
+  }
+
+
   render() {
+    
     return(
       <form onSubmit={this.props.handleSubmit}>
          <div>
@@ -72,28 +101,49 @@ class BookingForm extends React.Component {
         </div>
         <div>
         <label htmlFor="datetime">Appointment Time</label>
-        <DatePicker
-            selected={this.state.startDate}
-            onChange={this.handleChange}
-            locale="en-GB"
-            showTimeSelect
-            timeFormat="hh:mm aa"
-            timeIntervals={15}
-            dateFormat="dd-MMMM-yyyy hh:mm aa"
-            placeholderText="Click to select"
+        <Field
+            name="datetime"
+            component={this.datePicker}
+            value={this.state.startDate}
+            initialValues={this.state.startDate}
+            // value={this.state.startDate}
           />
         </div>
         <div>
-        <label htmlFor="firstName">Comments</label>
+        <label htmlFor="comments">Comments</label>
         <Field name="comments" component="input" type="text"/>
          </div>
          <button type="submit" disabled={this.props.pristine || this.props.submitting}>Submit</button>
       </form>
-     
-
+      
     )
   }
 }
 
+
 BookingForm = reduxForm({ form: 'booking', validate })(BookingForm);
+// const selector = formValueSelector('booking');
+// BookingForm = connect(state => {
+//   const datetimeValue = selector(state, 'datetime')
+//   return {
+//     datetimeValue,
+//   }
+// })(BookingForm);
+const selector = formValueSelector('booking'); // <-- same as form name
+BookingForm = connect(state => {
+  // // can select values individually
+  // const hasEmailValue = selector(state, 'hasEmail');
+  // const favoriteColorValue = selector(state, 'favoriteColor');
+  // or together as a group
+  const { firstName, lastName, email, phone, datetime, comments } = selector(state, 'firstName', 'lastName', 'email', 'phone', 'datetime', 'comments');
+  return {
+    firstName,
+    lastName,
+    email,
+    phone,
+    datetime,
+    comments
+  };
+})(BookingForm);
+
 export default BookingForm;
