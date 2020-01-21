8ed0e48e616f2966020842bc40aac689cfe35677
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-01-22 10:27:40 +1100

    added css to booking page

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -3,26 +3,30 @@ import Nav from "../components/Nav";
 import BookingForm from "../components/bookingform";
 
 import "react-datepicker/dist/react-datepicker.css";
-import "../css/Booking.css";
 
 class Booking extends React.Component {
-
- bookingSubmit = (values) => {
+  bookingSubmit = values => {
     console.log(values);
-
-  }
+  };
 
   render() {
     return (
       <>
         <Nav />
-        <BookingForm onSubmit={this.bookingSubmit} />
+        <div className="booking">
+          <div className="container">
+            <div className="imageBooking"></div>
+            <div className="content-booking">
+              <div>
+                <h1>Make An Appointment!</h1>
+                <BookingForm onSubmit={this.bookingSubmit} />
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
 }
 
 export default Booking;
-
-
-
