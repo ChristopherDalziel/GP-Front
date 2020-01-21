c5fc21ffebdddddb88223bf2a8384915f5028473
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-01-21 11:16:12 +1100

    Cleanup

---
+++ src/components/emailSubmit.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function handleSubmit(values) {
+  const {
+    first_name,
+    last_name,
+    contact_number,
+    subject,
+    email,
+    message
+  } = values;
+  try {
+    const res = await axios.post(process.env.REACT_APP_BACKEND_URL + "/send", {
+      first_name,
+      last_name,
+      contact_number,
+      subject,
+      email,
+      message
+    });
+    console.log(res);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+export default handleSubmit;
