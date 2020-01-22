5a973d5f2bceae6153c7794fea273de2584cd803
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-01-23 09:44:35 +1100

    added CSS to log-in screen

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,77 +1,80 @@
-import React, { Fragment,useState } from "react";
-import axios from 'axios';
-import { setLocalStorage } from '../utils/local-storage';
-import {Link} from 'react-router-dom';
-import EmailVerificationForm from '../components/emailverificationform';
-import LoginForm from '../components/loginform';
+import React, { Fragment, useState } from "react";
+import axios from "axios";
+import { setLocalStorage } from "../utils/local-storage";
+import { Link } from "react-router-dom";
+import EmailVerificationForm from "../components/emailverificationform";
+import LoginForm from "../components/loginform";
 import Nav from "../components/Nav";
 import "../css/signIn.css";
 
-const SignIn = (props) => { 
+const SignIn = props => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
   const [error, setError] = useState(null);
-  const [passwordReset, setPasswordReset] = useState(false)
+  const [passwordReset, setPasswordReset] = useState(false);
 
-const onSubmitLoginForm = async(e) => {
-  console.log({email, password})
-  try {
-    e.preventDefault();
-    const response = await axios.post(process.env.REACT_APP_BACKEND_URL + '/users/login', {
-      email,
-      password
-    } )
-    console.log(response.data)
-    setLocalStorage(response.data)
-    props.history.push('/')
-  } catch(err) {
-    setError({
-      msg: err.message
-    })
-  }
-}
+  const onSubmitLoginForm = async e => {
+    console.log({ email, password });
+    try {
+      e.preventDefault();
+      const response = await axios.post(
+        process.env.REACT_APP_BACKEND_URL + "/users/login",
+        {
+          email,
+          password
+        }
+      );
+      console.log(response.data);
+      setLocalStorage(response.data);
+      props.history.push("/");
+    } catch (err) {
+      setError({
+        msg: err.message
+      });
+    }
+  };
 
-const onSubmitEmailVerificationForm = async (e) => {
-  try {
-    e.preventDefault();
-    setError({
-      msg: 'An email has been sent with a link to reset your password'
-    })
-    await axios.put(process.env.REACT_APP_BACKEND_URL + '/mail/forgotten-password', {
-      email
-    })
-  } catch (err) {
-    console.log(err.message)
-  }
-}
+  const onSubmitEmailVerificationForm = async e => {
+    try {
+      e.preventDefault();
+      setError({
+        msg: "An email has been sent with a link to reset your password"
+      });
+      await axios.put(
+        process.env.REACT_APP_BACKEND_URL + "/mail/forgotten-password",
+        {
+          email
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
-if (passwordReset) {
-  return (
-    <EmailVerificationForm 
-      onSubmitEmailVerificationForm={onSubmitEmailVerificationForm}
-      setEmail={setEmail}
-      error={error}
-    />
-  )
-} else {
-  return ( 
-    <>
-      <Nav />
+  if (passwordReset) {
+    return (
+      <EmailVerificationForm
+        onSubmitEmailVerificationForm={onSubmitEmailVerificationForm}
+        setEmail={setEmail}
+        error={error}
+      />
+    );
+  } else {
+    return (
+      <>
+        <Nav />
         <div>
-        <LoginForm 
-          onSubmitLoginForm={onSubmitLoginForm}
-          setEmail={setEmail}
-          error={error}
-          setPassword={setPassword}
-          setPasswordReset={setPasswordReset}
-        />
-        <h4>Don't have an account? 
-        <Link to="register"> Sign Up Here</Link>
-        </h4>
-      </div>
-    </>
-   )
+          <LoginForm
+            onSubmitLoginForm={onSubmitLoginForm}
+            setEmail={setEmail}
+            error={error}
+            setPassword={setPassword}
+            setPasswordReset={setPasswordReset}
+          />
+        </div>
+      </>
+    );
   }
-}
+};
 
-export default SignIn
+export default SignIn;
