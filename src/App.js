b896447ec0d1959ef6a7d70f20bc41601cf9e979
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-01-22 15:03:09 +1100

    displaying vaccines from the mongo atlas db

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , {useEffect} from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
@@ -10,31 +10,24 @@ import Vaccines from "./pages/vaccines";
 import Services from "./pages/services";
 import Booking from "./pages/booking";
 
-
-
-
-const App =()=> {
-
- 
-    return (
-      <>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/register" component={Register} />
-            <Route path="/signIn" component={SignIn} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/about" component={About} />
-            <Route path="/vaccines" component={Vaccines} />
-            <Route path="/services" component={Services} />
-            <Route path="/booking" component={Booking} />
-          </Switch>
-        </BrowserRouter>
-      </>
-
-    );
-  
-}
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/register" component={Register} />
+          <Route path="/signIn" component={SignIn} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/about" component={About} />
+          <Route path="/vaccines" component={Vaccines} />
+          <Route path="/services" component={Services} />
+          <Route path="/booking" component={Booking} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
