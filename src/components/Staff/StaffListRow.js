dd7481bac627a755fec18be0f3a086e44cfb3a99
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-01-28 13:55:05 +1100

    admin dash changes

--- src/components/Staff/StaffListRow.js
+++ src/components/Staff/StaffListRow.js
@@ -4,35 +4,32 @@ import axios from "axios";
 
 export default class StaffListRow extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.deleteStaff = this.deleteStaff.bind(this);
-
   }
-  
 
   deleteStaff() {
-    axios.delete('http://localhost:5000/admin/delete_staff/' + this.props.obj._id)
-        .then((res) => {
-            console.log('Staff successfully deleted!')
-    
-        }).catch((error) => {
-            console.log(error)
-        })
+    axios
+      .delete("http://localhost:5000/admin/delete_staff/" + this.props.obj._id)
+      .then(res => {
+        console.log("Staff successfully deleted!");
+        window.location.replace("/admin_dashboard");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <tr>
+        <td>{this.props.obj.name}</td>
+        <td>{this.props.obj.aboutText}</td>
 
-}
-    render() {
-        return (
-            <tr>
-                <td>{this.props.obj.name}</td>
-                <td>{this.props.obj.aboutText}</td>
-                
-                <td>
-                    <Link to={"/admin/update_staff/" + this.props.obj._id}>
-                        Edit
-                    </Link>
-                    <button onClick={this.deleteStaff}>Delete</button>
-                </td>
-            </tr>
-        );
-    }
+        <td>
+          <Link to={"/admin/update_staff/" + this.props.obj._id}>Edit</Link>
+          <button onClick={this.deleteStaff}>Delete</button>
+        </td>
+      </tr>
+    );
+  }
 }
