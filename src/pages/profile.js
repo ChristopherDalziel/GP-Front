7329be662650b92bb3a7fc94e7b8142d9c5dcfc4
Nhan Dang <nhantdang@gmail.com>
2020-01-29 11:10:01 +1100

    edit user info completed

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,16 +2,19 @@ import React from "react";
 import axios from "axios";
 
 import "../css/Profile.css";
+import "../css/register.css";
 import UserAppointments from "../components/users/appointments";
-import RegistrationForm from "../components/authentication/registrationform";
+import UserInfoForm from "../components/users/editinfoform";
 
 class Profile extends React.Component {
 
   state = {
+    id: null,
     firstName: null,
     lastName: null,
     email: null,
-    phone: null
+    phone: null,
+    errors: null
   }
 
   componentDidMount() {
@@ -25,6 +28,7 @@ class Profile extends React.Component {
           .then(response => {
             const user = response.data;
             this.setState({
+              id: user.id,
               email: user.email,
               firstName: user.firstName,
               lastName: user.lastName,
@@ -38,6 +42,24 @@ class Profile extends React.Component {
     }
   }
 
+  editInfoSubmit = (values) => {
+    const id = this.state.id;
+    axios.patch(process.env.REACT_APP_BACKEND_URL + `/users/edit/${id}`, values).then((response) => {
+      console.log(response.data)
+      const {email, firstName, lastName, phone} = response.data;
+      this.setState({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phone: phone
+      })
+    })
+    .catch((err) => {
+      console.log(err.message);
+      this.setState({errors: err.message})
+    })
+  }
+
   render() {
     const { firstName, lastName, email, phone } = this.state;
     return (
@@ -48,10 +70,14 @@ class Profile extends React.Component {
               <UserAppointments />
           </div>
           <div className="container-profile">
-            Your Information
-            <div className="content-profile">
-              <RegistrationForm initialValues={{firstName: firstName, lastName: lastName, email: email, phone: phone}} />
-            </div>
+            <div className="content-signUp">
+              <h1>Your Information</h1>
+              <UserInfoForm onSubmit={this.editInfoSubmit} initialValues={{firstName: firstName, lastName: lastName, email: email, phone: phone}} />
+              <div>
+              {this.state.errors}
+              </div>
+              
+              </div>
           </div>
         </div>
       </>
