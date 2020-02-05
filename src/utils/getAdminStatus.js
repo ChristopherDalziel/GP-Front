8f95e48347ed3a5cbfd1e7038b54a18a15cb7184
Nhan Dang <nhantdang@gmail.com>
2020-02-05 23:40:06 +1100

    getadminstatus middlewre modified to use token in get request, local storage no longer stores admin boolean

--- src/utils/getAdminStatus.js
+++ src/utils/getAdminStatus.js
@@ -1,12 +1,18 @@
 // Function can be used to prevent users who do not have the admin credentials on their token from accessing pages
+import axios from "axios";
 
-function getAdminStatus() {
-  let adminVal = sessionStorage.getItem("admin");
-  if (adminVal === "true") {
+async function getAdminStatus() {
+ const response =  await axios.get(process.env.REACT_APP_BACKEND_URL + '/users/check-admin', { headers: { Authorization: sessionStorage.getItem("token") } })
+ console.log(response.data)
+ let adminVal;
+  if (response.data.admin === "true") {
     adminVal = true;
+  } else if (response.data.success === "false") {
+    adminVal = false;
   } else {
     adminVal = false;
   }
+  console.log(adminVal)
   return adminVal;
 }
 
--- src/utils/local-storage.js
+++ src/utils/local-storage.js
@@ -1,7 +1,7 @@
 
 export const setLocalStorage = (response) => {
   sessionStorage.setItem('token', response.token);
-  sessionStorage.setItem('admin', response.admin)
+  // sessionStorage.setItem('admin', response.admin)
   sessionStorage.setItem('loggedIn', true);
 
 }
